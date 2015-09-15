106d11d790126c4708dc8cb6ddccb8c85492ff85
Dan Kee <dan@dankee.com>
2015-09-14 23:13:10 -0500

    Stay on home row when moving windows.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -220,7 +220,7 @@
 
 (require 'cider)
 
-(global-set-key (kbd "C-x <up>") 'windmove-up)
-(global-set-key (kbd "C-x <down>") 'windmove-down)
-(global-set-key (kbd "C-x <right>") 'windmove-right)
-(global-set-key (kbd "C-x <left>") 'windmove-left)
+(global-set-key (kbd "C-c k") 'windmove-up)
+(global-set-key (kbd "C-c j") 'windmove-down)
+(global-set-key (kbd "C-c l") 'windmove-right)
+(global-set-key (kbd "C-c h") 'windmove-left)
