e94ffec27ccb5b0ae92f85312a49d921a91d7316
Dan Kee <dan@dankee.com>
2016-01-01 11:44:01 -0600

    Add lein-try.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,3 @@
-{:user {:plugins [[lein-droid "0.2.3"]]
+{:user {:plugins [[lein-droid "0.2.3"]
+                  [lein-try "0.4.3"] ]
         :android {:sdk-path "/usr/local/opt/android-sdk"} }}
-
