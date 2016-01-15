3e4f29293c4c2aa559f7baf9723fec1841c995e0
Dan Kee <dan@dankee.com>
2016-01-15 13:14:16 -0600

    Make cider happy.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,5 @@
 {:user {:plugins [[lein-droid "0.2.3"]
                   [lein-try "0.4.3"] ]
-        :android {:sdk-path "/usr/local/opt/android-sdk"} }}
+        :android {:sdk-path "/usr/local/opt/android-sdk"} }
+ :repl {:plugins [[cider/cider-nrepl "0.11.0-snapshot"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]] } }
