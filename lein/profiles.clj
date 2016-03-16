674defa119b478fd4aa4a4ea563ae30d7d45c005
Dan Kee <dan@dankee.com>
2016-03-16 04:03:38 +0000

    Update cider.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,4 @@
 {:user {:plugins [[lein-droid "0.2.3"]
                   [lein-try "0.4.3"] ]
         :android {:sdk-path "/usr/local/opt/android-sdk"} }
- :repl {:plugins [[cider/cider-nrepl "0.10.2"]]} }
+ :repl {:plugins [[cider/cider-nrepl "0.11.0"]]} }
