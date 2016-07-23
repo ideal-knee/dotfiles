8bcfc34a7cc63a013cac2d95e34b9ac9a80a8042
Dan Kee <dan@dankee.com>
2016-07-22 23:54:35 -0400

    Add lein auto.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-droid "0.2.3"]
-                  [lein-try "0.4.3"] ]
+                  [lein-try "0.4.3"]
+                  [lein-auto "0.1.2"] ]
         :android {:sdk-path "/usr/local/opt/android-sdk"} }
  :repl {:plugins [[cider/cider-nrepl "0.11.0"]]} }
