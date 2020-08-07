cca24a35fc4ac8b1e5cf05ceb1253b36c88f9746
Dan Kee <dan.kee@getbraintree.com>
2020-08-07 01:50:54 +0000

    Update MELPA URL

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -10,7 +10,7 @@
 (require 'package)
 (package-initialize)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 (defvar dkee/packages '(cider
                         clojure-mode
                         dockerfile-mode
