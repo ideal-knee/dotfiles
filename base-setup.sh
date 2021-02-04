8e47cb481c4550be43c9ead2ea2420d35e27936d
Dan Kee <dan@dankee.com>
2021-02-04 00:19:40 -0500

    Add base setup script

---
+++ base-setup.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+ln -s ~/Development/ideal-knee/dotfiles/emacs.d ~/.emacs.d
+ln -s ~/Development/ideal-knee/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/Development/ideal-knee/dotfiles/tmux.conf ~/.tmux.conf
